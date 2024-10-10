d2e80227b0f0c3ba226920731c48d6823a36bf6c
Student BE106 <Student.BE106@chaffey.edu>
2024-10-10 16:07:24 -0700

    Yoooo we can dash now

--- yellow-creatures/Scripts/player.gd
+++ yellow-creatures/Scripts/player.gd
@@ -6,15 +6,20 @@ const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var jumps : int
-
 var health : int
+var dash : int
+var hasDash : bool
+var usingDash : bool
+var dashTimerDone : bool
 
 func _ready():
 	$"Sprite2D/Player Animation".play("Idle")
 	jumps = 2
 	health = 3
+	dash = 20
+	hasDash = true
+	dash = 20
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
@@ -28,7 +33,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	#if is_on_floor():
+	#	hasDash = true
+	#	dash = 20
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		if jumps > 1:
@@ -54,6 +61,22 @@ func _physics_process(delta):
 		$"Sprite2D/Player Animation".play("Idle")
 
 	move_and_slide()	
+	if Input.is_key_pressed(KEY_H) and hasDash:
+		$DashTimer.start()
+		dashTimerDone = false
+		usingDash = true
+	if usingDash:
+		if dash>0:
+			self.position.x += dash * scale.y
+			dash-=1
+		if dash < 1:
+			hasDash = false
+			usingDash = false
+	if Input.is_key_pressed(KEY_G):
+		print("Test G")
+		var impulse_strength = 100
+		var angle = $RigidBody2D.rotation
+		$RigidBody2D.apply_central_impulse(Vector2(cos(angle), sin(angle)) * impulse_strength)
 
 func take_damage(damage:int):
 	health -= damage
@@ -68,3 +91,9 @@ func take_damage(damage:int):
 
 func _on_damage_timer_timeout():
 	$HealthBar.visible = false
+
+
+func _on_dash_timer_timeout() -> void:
+	hasDash = true
+	dashTimerDone = true
+	dash = 20
