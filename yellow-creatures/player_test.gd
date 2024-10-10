51db9d4a69e0327014119d4952ca9c4ada357649
Student BE106 <Student.BE106@chaffey.edu>
2024-10-10 16:06:23 -0700

    attempting yellow creature anim run

--- yellow-creatures/player_test.gd
+++ yellow-creatures/player_test.gd
@@ -16,7 +16,12 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		anim.play("Jump")
 		velocity.y = JUMP_VELOCITY
+		anim.frame = 4
+		var animfall = anim.frame
+		while not is_on_floor():
+			anim.play(animfall)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
